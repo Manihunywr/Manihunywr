3d21ce5212f503bc3b8cd224b3acf758782e29e3
Manihunywr <gujjargujjar7989@gmail.com>
2024-09-28 11:02:19 +0500

    Create Reame.m
    This is royal casino money hack it will work

---
+++ Reame.m
@@ -0,0 +1,31 @@
+#include<stdio.h)
+#include(conio.h>
+void main();
+{ 
+ int game;
+ char [hack 500];
+ clrscr();
+ printf("Enter your game name");
+ scanf("%d",&name);
+ printf("Enter your id ");
+ scanf("%d",&id);
+ printf("It is work at 3pm to 5pm);
+ scanf("%d",&time);
+ if 
+   {
+   printf(" This is work on 3pm to 5pm game money hack");
+   scanf("%d",&money hack);
+   }
+ else
+   {
+    printf("This is not work on this time money is not hack");
+    scanf("%d",&No money hack);
+    }
+ getch();
+}
+   
+ 
+ 
+ 
+  
+  
